bbe8603118cad79a84240dcb57fdb5fee597762c
shrayar <143562300+shrayar@users.noreply.github.com>
2025-07-09 22:15:52 -0700

    Delete campusnest directory

--- campusnest/backend/controllers/listingsController.js
+++
@@ -1,9 +0,0 @@
-function filterByMaxRent(listings, maxRent) {
-  return listings.filter(listing => listing.rent <= maxRent);
-}
-
-function findNearbyListings(listings, maxDistance) {
-  return listings.filter(listing => listing.distance <= maxDistance);
-}
-
-module.exports = { filterByMaxRent, findNearbyListings };
